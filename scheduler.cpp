8f820d57897bd1b317e404829d238f3479d93854
jaskaranbhatia25 <100167361+jaskaranbhatia25@users.noreply.github.com>
2022-11-07 15:15:06 -0700

    Update scheduler.cpp

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,130 +1,85 @@
-// this is the ONLY file you should edit and submit to D2L
+// this is the only file you should modify and submit for grading
 
-#include "deadlock_detector.h"
+#include "scheduler.h"
 #include "common.h"
-using string = std::string;
+#include "iostream"
+// this is the function you should implement
+//
+// runs Round-Robin scheduling simulator
+// input:
+//   quantum = time slice
+//   max_seq_len = maximum length of the reported executing sequence
+//   processes[] = list of process with populated IDs, arrival_times, and bursts
+// output:
+//   seq[] - will contain the execution sequence but trimmed to max_seq_len size
+//         - idle CPU will be denoted by -1
+//         - other entries will be from processes[].id
+//         - sequence will be compressed, i.e. no repeated consecutive numbers
+//   processes[]
+//         - adjust finish_time and start_time for each process
+//         - do not adjust other fields
+//
+void simulate_rr(
+    int64_t quantum, 
+    int64_t max_seq_len,
+    std::vector<Process> & processes,
+    std::vector<int> & seq
+) {
+    seq.clear();
 
-class FastGraph {
-public:
-    std::vector<std::vector<int>> adj_list;
-    std::vector<int> out_counts;
 
-} graph;
-
-std::vector<string> names;
-jaskaran
-std::vector<string> topological_sort()
-{
-    std::vector<int> out = graph.out_counts;
-    std::vector<int> zeros;
-
-    int i = 0;
-    for (auto o : out) {
-
-        // finds all nodes in graph with outdegree of 0
-        if (o == 0) {
-            zeros.push_back(i);
-        }
-
-        i++;
+    int64_t curr_time =0;
+    int64_t remaining_slice = quantum;
+    int cpu = -1;
+    std::vector<int> rq, jq;
+    std::vector<int64_t> remaining_bursts;
+    for(auto & process : processes){
+        jq.push_back(process.id);
+        remaining_bursts.push_back(process.burst);
     }
+   
+    while(1){
+    
+        cpu = -1;
 
-    while (!zeros.empty()) {
-
-        int n = zeros[zeros.size() - 1];
-        zeros.pop_back();
-
-        for (auto n2 : graph.adj_list[n]) {
-            out[n2]--;
-
-            if (out[n2] == 0) {
-                zeros.push_back(n2);
+        if(!rq.empty() && remaining_slice==0 ){
+            if( remaining_bursts[rq[0]]!=0 ){
+                remaining_slice = quantum;
+                int current_process = rq[0];
+                rq.erase(rq.begin());
+                rq.push_back(current_process);
             }
         }
-    }
+        
+        if(!jq.empty()&&processes[jq[0]].arrival_time==curr_time){
+            rq.push_back(jq[0]);
+            jq.erase(jq.begin());
+            continue;
+        }
+      
+        if(!rq.empty() && remaining_bursts[rq[0]]==0){
+            remaining_slice = quantum;
+            processes[rq[0]].finish_time = curr_time;
+            rq.erase(rq.begin());
 
-    std::vector<string> dl_procs;
-    i = 0;
-    for (auto n : out) {
+        }
+        
+        if(!rq.empty() && remaining_slice <= quantum && remaining_slice>0){
+            
+            
+            if(processes[rq[0]].start_time == -1) processes[rq[0]].start_time=curr_time;  
+            remaining_slice--;
+            cpu = rq[0];
+            remaining_bursts[rq[0]]--;  
+            
+        } 
+           
+        if(rq.empty() && jq.empty()) break;
+        if(!rq.empty() && !jq.empty()) 
+     
+        if(seq.back() !=cpu &&seq.push_back(cpu);
+        curr_time++;
 
-        if (n > 0) {
-            string str = names[i];
-
-            if (str[0] == 'p'){
-                dl_procs.push_back(str.erase(0, 2));
-            }
-        }
-        i++;
-    }
-
-    return dl_procs;
-}
-
-Result detect_deadlock(const std::vector<std::string> & edges)
-{
-    Result result;
-    int i = 0;
-    Word2Int word_to_int;
-    for (auto str : edges) {
-        std::vector<string> comps = split(str);
-        string process = "p_" + comps[0];
-        string arrow = comps[1];
-        string resource = "r_" + comps[2];
-        int p = word_to_int.get(process);
-        if (names.size() == p) {
-            names.push_back(process);
-        }
-        int r = word_to_int.get(resource);
-        if (names.size() == r) {
-            names.push_back(resource);
-        }
-        if (arrow == "<-") {
-            if (graph.adj_list.size() == p) {
-                graph.adj_list.push_back(std::vector<int>());
-                graph.adj_list[p].push_back(r);
-            }
-            else if (graph.adj_list.size() > p) {
-                graph.adj_list[p].push_back(r);
-            }
-            if (graph.adj_list.size() == r) {
-                graph.adj_list.push_back(std::vector<int>());
-            }
-            if (graph.out_counts.size() == p) {
-                graph.out_counts.push_back(0);
-            }
-            if (graph.out_counts.size() == r) {
-                graph.out_counts.push_back(0);
-            }
-            graph.out_counts[r]++;
-        }
-        else if (arrow == "->") {
-            if (graph.adj_list.size() == p) {
-                graph.adj_list.push_back(std::vector<int>());
-            }
-            if (graph.adj_list.size() == r) {
-                graph.adj_list.push_back(std::vector<int>());
-                graph.adj_list[r].push_back(p);
-            }
-            else if (graph.adj_list.size() > r) {
-                graph.adj_list[r].push_back(p);
-            }
-            if (graph.out_counts.size() == p) {
-                graph.out_counts.push_back(0);
-            }
-            if (graph.out_counts.size() == r) {
-                graph.out_counts.push_back(0);
-            }
-            graph.out_counts[p]++;
-        }
-        std::vector<string> topo_dl = topological_sort();
-        if (!topo_dl.empty()) {
-            result.dl_procs = topo_dl;
-            result.edge_index = i;
-            return result;
-        }
-        i++;
-    }
-    result.dl_procs.push_back("");
-    result.edge_index = -1;
-    return result;
+    } 
+    return;
 }
